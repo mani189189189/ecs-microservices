ec40d390550d1f7f45298b1f72d0dd3aebc003ac
mani189189189 <66417315+mani189189189@users.noreply.github.com>
2025-07-16 15:45:49 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,6 +10,24 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 
+resource "aws_s3_bucket" "tf_state" {
+  bucket = "ecs-"
+  versioning {
+    enabled = true
+  }
+}
+
+resource "aws_dynamodb_table" "tf_locks" {
+  name         = "terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
 ##############################
 # VPC & Networking
 ##############################

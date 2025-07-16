b835c99ec4548b44d5b1b50bd8d018a3a3273182
mani189189189 <66417315+mani189189189@users.noreply.github.com>
2025-07-16 15:54:20 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,27 +6,6 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 
-# S3 for remote state
-resource "aws_s3_bucket" "tf_state" {
-  bucket = "ecs-buckettfstore"
-
-  versioning {
-    enabled = true
-  }
-}
-
-# DynamoDB for state locking
-resource "aws_dynamodb_table" "tf_locks" {
-  name         = "terraform-locks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
-
 # VPC
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
